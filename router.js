550e8b1d10163fedd64357c7d0840ebffd325418
Michael John Isip <mj.isip123@gmail.com>
2020-02-11 15:00:35 +0800

    request resume

--- router.js
+++ router.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const fs = require('fs')
+const dotenv = require('dotenv')
+const nodemailer = require('nodemailer')
+
+dotenv.config()
 
 router.get('/', (req, res) => {
+    console.log(process.env.GOOGLE_MAIL)
     res.send(`Server is up and running...`)
 })
 
@@ -14,5 +19,37 @@ router.get('/data', async (req, res) => {
         res.json({ data })
     })
 })
+
+router.post('/resume/request', (req, res) => {
+
+    const name = req.body.name
+    const from = req.body.from
+    const body = req.body.message
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.GOOGLE_MAIL,
+            pass: process.env.GOOGLE_PASSWORD
+        }
+    });
+
+    const mailOptions = {
+        from: from,
+        to: process.env.GOOGLE_MAIL,
+        subject: 'Resume Request | ' + name,
+        text: body
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+            res.status(500);
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.status(201);
+        }
+    });
+} )
 
 module.exports = router
