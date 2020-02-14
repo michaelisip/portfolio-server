79c74d84b33be0ea166ec4ab3dd09276a7f315fc
Michael John Isip <mj.isip123@gmail.com>
2020-02-14 23:32:09 +0800

    eslint and error middlewares

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
-const express = require('express')
-const cors = require('cors')
-const helmet = require('helmet')
-const morgan = require('morgan')
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
-const router = require('./router')
+const router = require('./router');
+require('dotenv').config();
 
-const app = express()
-
-require('dotenv').config()
-const PORT = process.env.PORT || 5000
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: process.env.CORS_ORIGIN
-}))
-app.use(express.json())
-app.use(helmet())
-app.use(morgan('common'))
+  origin: process.env.CORS_ORIGIN,
+}));
+app.use(helmet());
+app.use(express.json());
+app.use(morgan('common'));
 
-app.use(router)
-app.use(express.static('public'))
+app.use(router);
+app.use(express.static('public'));
 
-app.listen(PORT, () => console.log(`Listening to port ${PORT}`))
+app.use((req, res, next) => {
+  const error = new Error(`Not Found | ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: error.message,
+    stack: process.env.ENV === 'production' ? '' : error.stack,
+  });
+});
+
+app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
--- router.js
+++ router.js
@@ -1,54 +1,51 @@
-const express = require('express')
-const router = express.Router()
-const fs = require('fs')
-const dotenv = require('dotenv')
-const nodemailer = require('nodemailer')
+/* eslint-disable no-console */
+const express = require('express');
+const fs = require('fs');
+const nodemailer = require('nodemailer');
 
-dotenv.config()
+const router = express.Router();
+require('dotenv').config();
 
 router.get('/', (req, res) => {
-    res.send(`Server is up and running...`)
-})
+  res.send('Server is up and running...');
+});
 
 router.get('/data', async (req, res) => {
-    fs.readFile('./data.json', (err, data) => {
-        if(err) res.status(500)
-
-        data = JSON.parse(data)
-        res.json({ data })
-    })
-})
+  fs.readFile('./data.json', (err, json) => {
+    if (err) res.status(500);
+    const data = JSON.parse(json);
+    res.json({ data });
+  });
+});
 
 router.post('/resume/request', (req, res) => {
 
-    const name = req.body.name
-    const from = req.body.email
-    const body = req.body.message
+  const { name, email, message } = req.body;
 
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.GOOGLE_MAIL,
-            pass: process.env.GOOGLE_PASSWORD
-        }
-    });
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.GOOGLE_MAIL,
+      pass: process.env.GOOGLE_PASSWORD,
+    },
+  });
 
-    const mailOptions = {
-        from: `${name} <${from}>`,
-        to: `MichaelJohn Isip <${process.env.GOOGLE_MAIL}>`,
-        subject: 'Resume Request | ' + name,
-        text: body
-    };
+  const mailOptions = {
+    from: `${name} <${email}>`,
+    to: `MichaelJohn Isip <${process.env.GOOGLE_MAIL}>`,
+    subject: `Resume Request | ${name}`,
+    text: message,
+  };
 
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-            console.log(error);
-            res.status(500);
-        } else {
-            console.log('Email sent: ' + info.response);
-            res.status(201);
-        }
-    });
-} )
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500);
+    } else {
+      console.log(`Email sent: ${info.response}`);
+      res.status(201);
+    }
+  });
+});
 
-module.exports = router
+module.exports = router;
