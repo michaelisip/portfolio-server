79cecb8f2e849bf438df3774801860b2f8c3f8a7
Michael John Isip <mj.isip123@gmail.com>
2020-02-14 22:40:43 +0800

    middlewares

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const cors = require('cors')
+const helmet = require('helmet')
+const morgan = require('morgan')
+
 const router = require('./router')
 
 const app = express()
@@ -7,8 +10,13 @@ const app = express()
 require('dotenv').config()
 const PORT = process.env.PORT || 5000
 
-app.use(cors())
+app.use(cors({
+  origin: process.env.CORS_ORIGIN
+}))
 app.use(express.json())
+app.use(helmet())
+app.use(morgan('common'))
+
 app.use(router)
 app.use(express.static('public'))
 
